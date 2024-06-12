422730f97b9d41267b5123ee4b60f66766ae3623
Stoyan Chompov <stoqnchompov@gmail.com>
2024-06-12 20:48:14 +0300

    create project

---
+++ TicketCenterL/src/main/java/bg/tuvarna/si/ticketcenterl/TicketCenterLApplication.java
@@ -0,0 +1,13 @@
+package bg.tuvarna.si.ticketcenterl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketCenterLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketCenterLApplication.class, args);
+    }
+
+}
---
+++ TicketCenterL/src/test/java/bg/tuvarna/si/ticketcenterl/TicketCenterLApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.tuvarna.si.ticketcenterl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketCenterLApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
